f8f4dd1b615d263568dddadc8b615118f88cb4d9
AberQ <egor.master2017@gmail.com>
2024-12-04 01:08:11 +0500

    Добавил Swagger-документацию

--- base/urls.py
+++ base/urls.py
@@ -18,12 +18,31 @@ from django.contrib import admin
 from django.urls import path
 from autorization.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
     path('api/get-or-create-user/', GetOrCreateUser.as_view(), name='get_or_create_user'),
     path('api/autorize/', AuthenticateUser.as_view(), name='authenticate_user'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
