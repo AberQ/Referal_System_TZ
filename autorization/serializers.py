0c6483be3161f3179a8c75d22ed14cada7639e19
AberQ <egor.master2017@gmail.com>
2024-12-03 22:47:24 +0500

    Теперь выводятся не id рефералов а их номера  телефонов

--- autorization/serializers.py
+++ autorization/serializers.py
@@ -5,7 +5,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('phone_number', 'auth_code')
-        
+
 class UserProfileSerializer(serializers.ModelSerializer):
     # Сериализуем поле referred_by (если есть)
     referred_users = serializers.SerializerMethodField()
@@ -16,7 +16,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def get_referred_users(self, obj):
         """
-        Возвращает список ID пользователей, которые были приглашены текущим пользователем.
+        Возвращает список номеров телефонов пользователей, которые были приглашены текущим пользователем.
         """
         referred_users = CustomUser.objects.filter(referred_by=obj)
-        return [user.id for user in referred_users]
+        return [user.phone_number for user in referred_users]
