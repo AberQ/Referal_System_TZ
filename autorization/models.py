9bb89f409309ed8a105bfb91827f106e4d5a1b42
AberQ <egor.master2017@gmail.com>
2024-12-03 19:54:13 +0500

    Код теперь уникален

--- autorization/models.py
+++ autorization/models.py
@@ -4,46 +4,38 @@ import random
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, phone_number, password=None, **extra_fields):
-        """
-        Создаем пользователя с указанным номером телефона, паролем и кодом
-        """
         if not phone_number:
             raise ValueError('The Phone Number field must be set')
-        
-        # Генерация 4-значного кода
-        auth_code = self.generate_auth_code()
-        
-        # Создаем пользователя
-        user = self.model(phone_number=phone_number, auth_code=auth_code, **extra_fields)
+        user = self.model(phone_number=phone_number, **extra_fields)
         
         if password:
             user.set_password(password)
         else:
             user.set_unusable_password()
         
+        # Генерация уникального кода для пользователя
+        user.auth_code = self.generate_unique_auth_code()
+        
         user.save(using=self._db)
         return user
 
     def create_superuser(self, phone_number, password=None, **extra_fields):
-        """
-        Создаем суперпользователя с номером телефона, паролем и кодом
-        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        
-        # Генерация 4-значного кода
-        auth_code = self.generate_auth_code()
-        
-        return self.create_user(phone_number, password, auth_code=auth_code, **extra_fields)
+        return self.create_user(phone_number, password, **extra_fields)
 
-    def generate_auth_code(self):
-        """Генерирует случайный 4-значный код"""
-        return str(random.randint(1000, 9999))
-
+    def generate_unique_auth_code(self):
+        """
+        Генерация уникального 4-значного кода.
+        """
+        while True:
+            code = str(random.randint(1000, 9999))  # Генерируем случайный 4-значный код
+            if not CustomUser.objects.filter(auth_code=code).exists():  # Проверяем, что код уникален
+                return code
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=15, unique=True)
-    auth_code = models.CharField(max_length=4, blank=True, null=True)  # Поле для 4-значного кода
+    auth_code = models.CharField(max_length=4, blank=True, null=True, unique=True)  # Поле для 4-значного кода
     password = models.CharField(max_length=128, blank=True, null=True)  # Сделаем пароль необязательным
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
