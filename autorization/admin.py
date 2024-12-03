b84168da806be874211d1f5c1e480077dadce981
AberQ <egor.master2017@gmail.com>
2024-12-03 20:00:00 +0500

    При созданиии пользователя для него автоматически генерируется 6-значный реферальный код

--- autorization/admin.py
+++ autorization/admin.py
@@ -21,10 +21,10 @@ class CustomUserAdmin(admin.ModelAdmin):
     form = CustomUserChangeForm
     add_form = CustomUserCreationForm
 
-    list_display = ('phone_number', 'auth_code', 'is_active', 'is_staff', 'is_superuser')
+    list_display = ('phone_number', 'auth_code', 'referral_code', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     search_fields = ('phone_number',)
-    readonly_fields = ('auth_code',)  # Поле auth_code будет доступно только для чтения в админке
+    readonly_fields = ('auth_code', 'referral_code')  # Оба кода будут доступны только для чтения в админке
 
     # Настроим поля для добавления нового пользователя
     add_fieldsets = (
@@ -33,10 +33,12 @@ class CustomUserAdmin(admin.ModelAdmin):
         }),
     )
 
-    # Переопределяем save_model, чтобы генерировать auth_code при создании пользователя
+    # Переопределяем save_model, чтобы генерировать auth_code и referral_code при создании пользователя
     def save_model(self, request, obj, form, change):
-        if not obj.auth_code:  # Если код еще не был сгенерирован
-            obj.auth_code = CustomUserManager().generate_auth_code()  # Генерируем новый код
+        if not obj.auth_code:  # Если auth_code еще не был сгенерирован
+            obj.auth_code = CustomUserManager().generate_unique_auth_code()  # Генерация уникального auth_code
+        if not obj.referral_code:  # Если referral_code еще не был сгенерирован
+            obj.referral_code = CustomUserManager().generate_unique_referral_code()  # Генерация уникального referral_code
         super().save_model(request, obj, form, change)
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- autorization/models.py
+++ autorization/models.py
@@ -4,6 +4,9 @@ import random
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, phone_number, password=None, **extra_fields):
+        """
+        Создаем пользователя с номером телефона и паролем
+        """
         if not phone_number:
             raise ValueError('The Phone Number field must be set')
         user = self.model(phone_number=phone_number, **extra_fields)
@@ -13,13 +16,17 @@ class CustomUserManager(BaseUserManager):
         else:
             user.set_unusable_password()
         
-        # Генерация уникального кода для пользователя
+        # Генерация уникальных кодов
         user.auth_code = self.generate_unique_auth_code()
+        user.referral_code = self.generate_unique_referral_code()
         
         user.save(using=self._db)
         return user
 
     def create_superuser(self, phone_number, password=None, **extra_fields):
+        """
+        Создаем суперпользователя с номером телефона и паролем
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(phone_number, password, **extra_fields)
@@ -29,14 +36,23 @@ class CustomUserManager(BaseUserManager):
         Генерация уникального 4-значного кода.
         """
         while True:
-            code = str(random.randint(1000, 9999))  # Генерируем случайный 4-значный код
+            code = str(random.randint(1000, 9999))  # Генерация случайного 4-значного кода
             if not CustomUser.objects.filter(auth_code=code).exists():  # Проверяем, что код уникален
+                return code
+
+    def generate_unique_referral_code(self):
+        """
+        Генерация уникального 6-значного реферального кода.
+        """
+        while True:
+            code = str(random.randint(100000, 999999))  # Генерация случайного 6-значного кода
+            if not CustomUser.objects.filter(referral_code=code).exists():  # Проверяем, что код уникален
                 return code
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=15, unique=True)
-    auth_code = models.CharField(max_length=4, blank=True, null=True, unique=True)  # Поле для 4-значного кода
-    password = models.CharField(max_length=128, blank=True, null=True)  # Сделаем пароль необязательным
+    auth_code = models.CharField(max_length=4, blank=True, null=True, unique=True)  # Уникальный код
+    referral_code = models.CharField(max_length=6, blank=True, null=True, unique=True)  # Уникальный реферальный код
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -50,7 +66,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         return self.phone_number
 
     def save(self, *args, **kwargs):
-        # Если у пользователя нет пароля, устанавливаем невозможный
         if not self.password:
             self.set_unusable_password()
         super().save(*args, **kwargs)
