576583496de2c5f99911e497ba37b94966d45411
AberQ <egor.master2017@gmail.com>
2024-12-03 19:51:42 +0500

    Теперь код автоматически появляется при создании пользователя

--- autorization/admin.py
+++ autorization/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django import forms
-from .models import CustomUser
+from .models import *
 from django.contrib.auth.forms import UserChangeForm
 
 class CustomUserCreationForm(forms.ModelForm):
@@ -21,15 +21,22 @@ class CustomUserAdmin(admin.ModelAdmin):
     form = CustomUserChangeForm
     add_form = CustomUserCreationForm
 
-    list_display = ('phone_number', 'is_active', 'is_staff', 'is_superuser')
+    list_display = ('phone_number', 'auth_code', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     search_fields = ('phone_number',)
-    readonly_fields = ('auth_code',)
+    readonly_fields = ('auth_code',)  # Поле auth_code будет доступно только для чтения в админке
+
     # Настроим поля для добавления нового пользователя
     add_fieldsets = (
         (None, {
             'fields': ('phone_number', 'is_active', 'is_staff', 'is_superuser'),
         }),
     )
+
+    # Переопределяем save_model, чтобы генерировать auth_code при создании пользователя
+    def save_model(self, request, obj, form, change):
+        if not obj.auth_code:  # Если код еще не был сгенерирован
+            obj.auth_code = CustomUserManager().generate_auth_code()  # Генерируем новый код
+        super().save_model(request, obj, form, change)
 
 admin.site.register(CustomUser, CustomUserAdmin)
