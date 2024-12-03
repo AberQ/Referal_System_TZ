3dbfca0f7aa7aa518123b7ef768cdca800d6ae4b
AberQ <egor.master2017@gmail.com>
2024-12-03 19:32:19 +0500

    Исправил ошибку из-за которой нельзя было войти в админку

--- autorization/authentication.py
+++ autorization/authentication.py
@@ -1,20 +1,34 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.backends import ModelBackend
 from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('phone_number', 'is_active', 'is_staff', 'is_superuser')
+class PhoneAuthBackend(ModelBackend):
+    def authenticate(self, request, phone_number=None, code=None, password=None):
+        try:
+            # Находим пользователя по номеру телефона
+            user = CustomUser.objects.get(phone_number=phone_number)
+            
+            # Для администраторов проверяем пароль, если он задан
+            if user.is_staff and password:
+                if not user.check_password(password):  # Для администраторов проверяем пароль
+                    return None
+            
+            # Для обычных пользователей проверяем код, если он задан
+            if not user.is_staff and code:
+                session_code = request.session.get('auth_code')  # Код, который был сгенерирован и сохранен в сессии
+                if session_code != code:
+                    return None
 
-    def clean_password1(self):
-        # Возвращаем пустое значение, чтобы не требовать пароль
-        return None
+            # Если все проверки прошли успешно, возвращаем пользователя
+            return user
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if not user.password:
-            user.set_unusable_password()  # Устанавливаем невозможный пароль
-        if commit:
-            user.save()
-        return user
+        except CustomUser.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        """
+        Получаем пользователя по его ID
+        """
+        try:
+            return CustomUser.objects.get(pk=user_id)
+        except CustomUser.DoesNotExist:
+            return None
